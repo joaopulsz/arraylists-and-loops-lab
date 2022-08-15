50863a0c3b363a53e1f3804d5ac036a2f8fdce83
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-08-15 16:27:27 +0100

    finished numbers exercise

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -49,14 +49,51 @@ public class Runner {
         System.out.println("numbers: " + numbers);
 
 //        1. Print out a list of the even integers
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                System.out.println(number);
+            }
+        }
+
 //        2. Print the difference between the largest and smallest value
+        int largestValue = Collections.max(numbers);
+        int smallestValue = Collections.min(numbers);
+        System.out.println(largestValue - smallestValue);
+
 //        3. Print True if the list contains a 1 next to a 1 somewhere.
-//        4. Print the sum of the numbers,
+        for (int i = 0; i < numbers.size() - 1; i ++) {
+            for (int j = 0; j < numbers.size(); j++) {
+                if (numbers.get(i) == numbers.get(j)) {
+                    if (i == 1 && j == 1) {
+                        System.out.println(true);
+                    }
+                }
+            }
+        }
+
+//        4. Print the sum of the numbers
+        int total = 0;
+        for (int number : numbers) {
+            total += number;
+        }
+        System.out.println(total);
+
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count.
 //
-//          So [7, 13, 2] would have sum of 9.
+//          So [7, 13, 2] would have sum of 7.
+
+        int total2 = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i != 0) { //this avoids the out of bound error
+                if (numbers.get(i) == 13 || numbers.get(i -1) == 13 ) {
+                    total2 -= numbers.get(i);
+                }
+            }
+            total2 += numbers.get(i);
+        }
+        System.out.println(total2);
 
     }
 
